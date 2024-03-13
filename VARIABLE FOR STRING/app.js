3980163bd8f55f968054850e8bc660bd7cfa09d2
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-14 04:10:56 +0500

    VARIABLE FOR STRING

---
+++ VARIABLE FOR STRING/app.js
@@ -0,0 +1,11 @@
+var firstName = "Jhone ";
+var lastName = "Doe";
+alert(firstName + lastName);
+
+var age = 15;
+alert(age + " years old");
+
+alert("Certified Mobile Application Development")
+
+var  lineBreak = "\n";
+alert("pizza" + lineBreak + "pizz" + lineBreak + "piz" + lineBreak + "pi" + lineBreak + "p")
