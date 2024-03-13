9a77233e0d63f243b7747d7bf171eeff5324a22c
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-14 03:35:31 +0500

    alert chapter

--- ALERTS/app.js
+++ ALERTS/app.js
@@ -2,4 +2,10 @@ alert("ERROR! Please enter a valid email address");
 
 alert("welcome to JS Land...\nHappy Coding");
 
+alert("welcome to JS Land...");
+
+alert("Happy Coding\nprevent the this page from  creating additional dialogs.");
+
+alert("Hello... I can run JS through my web browser's console");
+
 
