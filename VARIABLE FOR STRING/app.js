b2ae4eadd3e255a348d04dffc19feb6727d45fde
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-14 04:27:59 +0500

    VARIABLES FOR STRING

--- VARIABLE FOR STRING/app.js
+++ VARIABLE FOR STRING/app.js
@@ -5,11 +5,31 @@ alert(firstName + lastName);
 var age = 15;
 alert(age + " years old");
 
-alert("Certified Mobile Application Development")
+alert("Certified Mobile Application Development");
 
-var  lineBreak = "\n";
-alert("pizza" + lineBreak + "pizz" + lineBreak + "piz" + lineBreak + "pi" + lineBreak + "p")
+var lineBreak = "\n";
+alert(
+  "pizza" +
+    lineBreak +
+    "pizz" +
+    lineBreak +
+    "piz" +
+    lineBreak +
+    "pi" +
+    lineBreak +
+    "p"
+);
 
 var email = "hb0485509@gmail.com";
-alert("My email address is " + email)
+alert("My email address is " + email);
 
+alert(
+  "iam trying to learn from tha Book A smarter  way to to learn JAVASCRIPT"
+);
+
+document.write(
+  "<h1>" + "Yah! I can write HTML content on JAVASCRIPT" + "</h1>"
+);
+
+var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(symbol);
