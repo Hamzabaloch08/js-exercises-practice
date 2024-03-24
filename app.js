65770309171015fa6b7c9b60c25583bb63f3f92d
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-24 23:40:15 +0500

    js first assignment

---
+++ app.js
@@ -0,0 +1,57 @@
+//alert 
+
+// alert("ERROR! Please enter a valid email address");
+
+// alert("welcome to JS Land...\nHappy Coding");
+
+// alert("welcome to JS Land...");
+
+// alert("Happy Coding\nprevent the this page from  creating additional dialogs.");
+
+// alert("Hello... I can run JS through my web browser's console");
+
+
+
+
+
+                                                         //variable for string
+
+
+// var firstName = "Jhone ";
+// var lastName = "Doe";
+// alert(firstName + lastName);
+
+// var age = 15;
+// alert(age + " years old");
+
+// alert("Certified Mobile Application Development");
+
+// var lineBreak = "\n";
+// alert(
+//   "pizza" +
+//     lineBreak +
+//     "pizz" +
+//     lineBreak +
+//     "piz" +
+//     lineBreak +
+//     "pi" +
+//     lineBreak +
+//     "p"
+// );
+
+// var email = "hb0485509@gmail.com";
+// alert("My email address is " + email);
+
+// alert(
+//   "iam trying to learn from tha Book A smarter  way to to learn JAVASCRIPT"
+// );
+
+// document.write(
+//   "<h1>" + "Yah! I can write HTML content on JAVASCRIPT" + "</h1>"
+// );
+
+// var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(symbol);
+
+// //end
+
