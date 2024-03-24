bbebed50d7fbf20ed783b606965cf76786308961
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-25 02:38:04 +0500

    chapter 1 to 5 assingment

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //alert 
 
+
+
 // alert("ERROR! Please enter a valid email address");
 
 // alert("welcome to JS Land...\nHappy Coding");
@@ -9,6 +11,8 @@
 // alert("Happy Coding\nprevent the this page from  creating additional dialogs.");
 
 // alert("Hello... I can run JS through my web browser's console");
+
+
 
 
 
@@ -56,11 +60,10 @@
 // //end
 
 
+
+
+
                                                //variables for numbes 
-
-
-
-
 
 // var age = 15;
 // alert("I am " + age + " years old");
@@ -82,3 +85,173 @@
 // var quantity = 5;
 // var product = "T-Shirt"
 // document.write("<b>" + visitorName + "</b>" + " ordered " + "<b>" + quantity + product + "</b>" + "(s) on XYZ Clothing store");
+
+
+
+                             //VARIABLES NAMES: Legal & illegle
+
+// == Q-1 ==
+// var statement = a = 10, b = 20, c = 30;
+// console.log(a,b,c)
+
+// == Q-2 ==
+// var legalName = myVar, my_name, $hamza, _hamza , hamza;
+// var illegalName = 3Myvar, Hamza, var , alert, console ;
+
+// == Q-3 ==
+// document.write("A heading stating “Rules for naming JS variables” </br>")
+// document.write("'A'.Variable names can only contain $sign, _underscore, letters  and numbers. </br>")
+// document.write("'B' .Variables must begin with a smallLetters, numbers or $sign and _underScore.")
+// document.write("'C' .Variable Names Are Case Sensitive.</br>");
+// document.write("'D' .Variable names should not be JS Predefined Keywords.");
+// ****************** Chapter-4-End ****************
+
+                          // match  Expression
+
+// ==== Q-1 ====
+// var incr = 3 + 5
+// var sum = incr
+// console.log(sum)
+
+// ==== Q-2 ====
+// ==== Substract ====
+// var subs = 5 - 3
+// var sum = subs
+// console.log(sum)
+
+// ==== Multi ====
+// var mult = 5 * 3
+// var sum = mult
+// console.log(sum)
+
+// ==== Division ====
+// var div = 5 / 3
+// var sum = div
+// console.log(sum)
+
+// ==== Modulus / Reminder ====
+// var rem = 10 % 3;
+// var sum = rem ;
+// console.log(sum)
+
+// ==== Q-3 ====
+// (A)
+// var myVar;
+// // (B)
+// document.write("value after declaration is ", myVar, "</br>");
+// // (C)
+// myVar = 5;
+// // (D)
+// document.write("initial Value of variable is ", myVar, "</br>");
+// // (E)
+// myVar = myVar + 1;
+// // (F)
+// document.write("value after increment is ", myVar, "</br>");
+// // (G)
+// myVar = myVar + 7;
+// // (H)
+// document.write("value after addition is ", myVar, "</br>");
+// // (I)
+// myVar = myVar - 1;
+// // (J)
+// document.write("value after decrement is ", myVar, "</br>");
+// // (K)
+// myVar = myVar % 3
+// // (L)
+// document.write("Output : “The remainder is :", myVar," ","“")
+
+// ==== Q-4 ====
+// var ticketPrice = 600;
+// ticketPrice = ticketPrice * 5;
+// document.write("total cost to buy 5 tickets of movie is ",ticketPrice," ","PKR")
+
+// ==== Q-5 ====
+ 
+// var table = "Table of 4 <br><br>";
+// document.write(table)
+// var four1 = 4;
+// document.write("4*1=", four1, "<br>");
+// document.write("4*2=", four1 * 2, "<br>");
+// document.write("4*3=", four1 * 3, "<br>");
+// document.write("4*4=", four1 * 4, "<br>");
+// document.write("4*5=", four1 * 5, "<br>");
+// document.write("4*6=", four1 * 6, "<br>");
+// document.write("4*7=", four1 * 7, "<br>");
+// document.write("4*8=", four1 * 8, "<br>");
+// document.write("4*9=", four1 * 9, "<br>");
+// document.write("4*10=", four1 * 10, "<br>") ;
+
+// ==== Q-6 ====
+// The Temperature Converter: It’s hot out! Let’s make a
+// converter based on the steps here.
+
+// ==== Q-7 ====
+// var heading = "<h1>Shopping Cart</h1>";
+// document.write(heading, "</br>");
+// var item1Price = 650;
+// document.write("Price OF Item 1:: ", item1Price, "</br>");
+// var quantityItem1 = 3;
+// document.write("Quantity OF Item 1:: ", quantityItem1, "</br>");
+// var item2Price = 100;
+// document.write("Price OF Item 2:: ", item2Price, "</br>");
+// var quantityItem2 = 7;
+// document.write("Quantity OF Item 2:: " , quantityItem2  ,"</br>")
+// var shippingCharges = 100;
+// document.write("Shipping Charges:: " , shippingCharges  ,"</br>")
+// var total =
+//   item1Price * quantityItem1 + item2Price * quantityItem2 + shippingCharges;
+// document.write("Total Cost Of Your Order Is ", total);
+
+// ==== Q-8 ====
+// var markSheet = "<h1> Mark Sheet </br> </h1>";
+// var totalMarks = 980;
+// document.write("Total Marks:: " , totalMarks,"</br>")
+// var markObtained = 804;
+// document.write("Total Marks:: " , markObtained,"</br>")
+// var calculation = markObtained * 100 / totalMarks
+// var percentage = calculation
+// document.write("Percentage:" , " ", percentage)
+
+// ==== Q-9 ====
+// var currency = "<h1> Currency in Pkr </br> </h1>";
+// document.write(currency);
+// var usDollar = 10;
+// var usdRate = 104.80;
+// var saRiyal = 25;
+// var sarRate = 28;
+// var result = usDollar * usdRate + saRiyal * sarRate;
+// document.write("Total Currency In PKR :"," ",result)
+
+// ==== Q-10 ====
+// var myNumber = 5
+// myNumber = (myNumber + 5) * 10 / 2 ;
+// document.write(myNumber)
+
+// ==== Q-11 ====
+// var ageCalculator = "<h1> Age Calculator </br> </h1>";
+// document.write(ageCalculator)
+// var currentYear = 2016
+// document.write("Current Year :",currentYear,"</br>")
+// var birthYear = 1992;
+// document.write("BirthYear Is : ",birthYear,"</br>");
+// var ageCalculate = currentYear - birthYear;
+// document.write("Your Age Is : ",ageCalculate)
+
+// ==== Q-12 ====
+// 12. The Geometrizer: Calculate properties of a circle.
+// a. Store a radius into a variable.
+
+// ==== Q-13 ====
+// var chocoTitle = "<h1>The Lifetime Supply Calculator: </h1>";
+// document.write(chocoTitle , "</br>")
+// var fSnack = "Chocolate Chip By Lu";
+// document.write("Favourite Snack :" ,fSnack, "</br>")
+// var cAge = 15;
+// document.write("Current Age IS: ",cAge, "</br>")
+// var mAge = 65;
+// document.write("estimated maximum  : ",mAge, "</br>")
+// var amountConsume = 3;
+// document.write("Amount of Snack PerDay You Consume: ",amountConsume,"</br>")
+// var totalConsumeTillLife = amountConsume * 30 * 12 *  (mAge - cAge)
+// var result = "You will need " + totalConsumeTillLife + " " + fSnack + " "  + "to Consume Till The age OF :" + mAge
+// document.write(result)   
